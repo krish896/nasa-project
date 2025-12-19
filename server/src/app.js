cb70e3c87e6cecdf3e0a97ab242a0bec21766f2d
Krish Kohli <krishkohli700@gmail.com>
2025-12-20 04:48:28 +0530

    edit app.js file route first and then others

--- server/src/app.js
+++ server/src/app.js
@@ -17,8 +17,8 @@ app.use(
 app.use(morgan("combined"));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/v1", api);
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
